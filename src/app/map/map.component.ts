09f79a1b41f2c60f23a8268647d3a12c9245b0b0
matthewberes <mattyberes@gmail.com>
2025-04-15 11:42:40 -0400

    create map and add pin dropping

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,50 @@
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
+import { marker } from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements AfterViewInit {
+  private map: any;
+  markerIcon = {
+    icon: L.icon({
+      iconSize: [25, 41],
+      iconAnchor: [10, 41],
+      popupAnchor: [2, -40],
+      // specify the path here
+      iconUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png",
+      shadowUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-shadow.png"
+    })
+  };
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+
+    this.map.on("click", (e: any) => {
+      console.log(e.latlng); // get the coordinates
+      L.marker([e.latlng.lat, e.latlng.lng], this.markerIcon).addTo(this.map);
+    });
+  }
+}
